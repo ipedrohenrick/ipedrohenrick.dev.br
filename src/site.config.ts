a3bf0b561882e54910368fc925bee7dd27c3c450
Pedro Henrick <pedrohenrickqa@gmail.com>
2025-10-29 17:25:17 -0300

    first commit

---
+++ src/site.config.ts
@@ -0,0 +1,8 @@
+const menuLinks: { path: string, title: string }[] = [
+  { path: "/", title: "Home" },
+  { path: "/blog", title: "Blog" },
+  { path: "/projects", title: "Projetos" },
+  { path: "/about", title: "About" },
+];
+
+export { menuLinks };
