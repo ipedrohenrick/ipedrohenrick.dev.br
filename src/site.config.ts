f7b3dd77689d9615b2c14057b8248021423e7915
Pedro Henrick <pedrohenrickqa@gmail.com>
2025-10-29 18:04:50 -0300

    feat: add header

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,7 +1,7 @@
 const menuLinks: { path: string, title: string }[] = [
   { path: "/", title: "Home" },
   { path: "/blog", title: "Blog" },
-  { path: "/projects", title: "Projetos" },
+  // { path: "/projects", title: "Projetos" },
   { path: "/about", title: "About" },
 ];
 
