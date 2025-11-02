da19fd27e0a7275e42bae6d8d60a01fb42305921
Pedro Henrick <pedrohenrickqa@gmail.com>
2025-11-02 18:08:17 -0300

    chore: config deployment domain

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ipedrohenrick.dev.br',
+  base: '/',
+  trailingSlash: 'always',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
